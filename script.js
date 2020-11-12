679da19e1c81ccc2dd354e1e2f018494615209f0
l1lYa <74378363+l1lYa@users.noreply.github.com>
2020-11-13 03:30:06 +0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+var page = document.querySelector(".page");
+console.log (page)
+var themeButton = document.querySelector('.theme-button');
+
+function a() {
+	page.classList.toggle('d-t');
+	page.classList.toggle('l-t');
+  
+};
